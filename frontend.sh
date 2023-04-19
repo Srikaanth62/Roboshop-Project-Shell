44296783fc41d272178f4cd917a69ac15f37271a
srikaanth <srikaanth62@gmail.com>
2023-04-19 10:42:58 +0530

    Frontend Server

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,16 @@
+echo -e "\e[33m>>>>>>> Install Nginx <<<<<<<<<\e[0m"
 yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[33m>>>>>>> Download frontend source code <<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[33m>>>>>>> Remove html folder <<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[33m>>>>>>> change path to html <<<<<<<<\e[0m"
 cd /usr/share/nginx/html
+echo -e "\e[33m>>>>>>> unzip frontend folder <<<<<<<\e[0m"
 unzip /tmp/frontend.zip
+echo -e "\e[33m>>>>>> copy roboshop conf file <<<<<<<\e[0m"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[33m>>>>>> enable and start nginx <<<<<<<<\e[0m"
 systemctl enable nginx
-systemctl start nginx
 systemctl restart nginx
 
