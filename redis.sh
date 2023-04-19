934c2d60b2b516d08171f9eafb79ea9bd13d2fb3
srikaanth <srikaanth62@gmail.com>
2023-04-19 12:02:33 +0530

    Mongodb Server

--- redis.sh
+++ redis.sh
@@ -1,7 +1,13 @@
+echo -e "\e[33m>>>>>>>> Install redis repo file <<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[33m>>>>>>>> Enable Redis 6.2 from package streams <<<<<<<<<\e[0m"
 dnf module enable redis:remi-6.2 -y
+echo -e "\e[33m>>>>>>>> Install redis <<<<<<<<<\e[0m"
 yum install redis -y
+echo -e "\e[33m>>>>>>>> update listen address <<<<<<<\e[0m"
+Sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 ## Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
+echo -e "\e[33m>>>>>>>>> Enable and restart the redis service<<<<<<<<<\e[0m"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
 
