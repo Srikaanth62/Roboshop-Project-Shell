c0b92d916b190beb25b1f3610b01db49d1a3f227
srikaanth <srikaanth62@gmail.com>
2023-04-19 13:32:53 +0530

    user server

--- user.sh
+++ user.sh
@@ -1,18 +1,33 @@
+echo -e "\e[34m>>>>>>>> Setup nodejs repo <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[34m>>>>>>>> Install nodejs <<<<<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[34m>>>>>>>> Add application user <<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e{34m>>>>>>> create app directory <<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e34m>>>>>>>> Download the application code <<<<<<\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cp user.service /etc/systemd/system/user.service
 cd /app
+echo -e "\e[34m>>>>>>>>> unzip user code file <<<<<<\e[0m"
 unzip /tmp/user.zip
+echo -e "\e[34m>>>>>>>> Install nodejs dependencies <<<<<<<<<\e[0m"
 npm install
+echo -e "\e34m>>>>>>> copy user service <<<<<<<<\e[0m"
+cp /root/Roboshop-Project-Shell/user.service /etc/systemd/system/user.service
+echo -e "\e34m>>>>>>> load service <<<<<<<<\e[0m"
 systemctl daemon-reload
+echo -e "\e34m>>>>>>> enable and start service <<<<<<<<\e[0m"
 systemctl enable user
 systemctl start user
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e34m>>>>>>> copy mongodb repo file <<<<<<<<\e[0m"
+cp /root/Roboshop-Project-Shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e34m>>>>>>> Install mongodb client <<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-mongo --host mongodb.srikaanth62.online </app/schema/user.js
+echo -e "\e34m>>>>>>> load mongodb schema <<<<<<<<\e[0m"
+mongo --host mongodb-dev.srikaanth62.online </app/schema/user.js
+echo -e "\e34m>>>>>>> restart user service <<<<<<<<\e[0m"
+systemctl restart user
 
 
 
