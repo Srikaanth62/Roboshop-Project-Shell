8fe864cbfae289d262769588b19ad5a9010dfab0
srikaanth <srikaanth62@gmail.com>
2023-04-19 15:59:51 +0530

    Dispatch server

--- dispatch.sh
+++ dispatch.sh
@@ -1,14 +1,24 @@
+echo -e "\e[32m>>>>>>>>> Install golang <<<<<<<<<<\e[0m"
 yum install golang -y
+echo -e "\e[32m>>>>>>>>>>Add application user<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[32m>>>>>>>>> create app directory <<<<<<<<<<\e[0m"
+rm-rf /app
 mkdir /app
+echo -e "\e[32m>>>>>>>>> download application code <<<<<<<<<<\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cp dispatch.service /etc/systemd/system/dispatch.service
+echo -e "\e[32m>>>>>>>>> unzip code file <<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/dispatch.zip
+echo -e "\e[32m>>>>>>>>> Install dependencies and build the applications <<<<<<<<<<\e[0m"
 go mod init dispatch
 go get
 go build
+echo -e "\e[32m>>>>>>>>> copy dispatch service <<<<<<<<<<\e[0m"
+cp /root/Roboshop-Project-Shell/dispatch.service /etc/systemd/system/dispatch.service
+echo -e "\e[32m>>>>>>>>> load service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
+echo -e "\e[32m>>>>>>>>> Enable and restart the service <<<<<<<<<<\e[0m"
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
 
