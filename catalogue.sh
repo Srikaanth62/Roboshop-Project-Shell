5e87e203f9af4f488bac3abe0628bcc6799490ca
srikaanth <srikaanth62@gmail.com>
2023-04-19 11:49:24 +0530

    Mongodb Server

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,30 @@
+echo -e "\e[32m>>>>>>> Setup nodejs repos <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[32m>>>>>>> Install Nodejs <<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[32m>>>>>>> add application user <<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[32m>>>>>> create app directory <<<<<<\e[0m"
 mkdir /app
+echo -e "\e[32m>>>>>> download application code <<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cp catalogue.service /etc/systemd/system/catalogue.service
 cd /app
+echo -e "\e[32m>>>>>> unzip application code <<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+echo -e "\e[32m>>>>>>>> Install nodejs dependencies <<<<<<<<\e[0m"
 npm install
+echo -e "\e[32m>>>>>>>> copy catalogue service file <<<<<<<<\e[0m"
+cp /root/Roboshop-Project-Shell/catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[32m>>>>>> load the service <<<<<<<<<\e[0m"
 systemctl daemon-reload
+echo -e "\e[32m>>>>>>> Enable and start the service <<<<<<<<\e[0m"
 systemctl enable catalogue
 systemctl start catalogue
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[32m>>>>>> copy and Setup mongodb repo <<<<<<<\e[0m"
+cp /root/Roboshop-Project-Shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[32m>>>>>>>>> Install mongodb client <<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-mongo --host mongodb.srikaanth62.online </app/schema/catalogue.js
+echo -e "\e[32m>>>>>>>>>> load schema <<<<<<<<<<<<<\e[0m"
+mongo --host mongodb-dev.srikaanth62.online </app/schema/catalogue.js
+echo -e "\e[32m>>>>>>>>>> Restart the catalogue service <<<<<<<<<\e[0m"
 systemctl restart catalogue
