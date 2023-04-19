f1e697d21ef30decbc7e720b85a436e1105e417f
srikaanth <srikaanth62@gmail.com>
2023-04-19 15:28:44 +0530

    Shipping server

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,9 +1,15 @@
+echo -e "\e[32m>>>>>>>>> Install YUM repo file <<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+echo -e "\e[32m>>>>>>>> Install erlang <<<<<<<<<<<<<<\e[0m"
 yum install erlang -y
+echo -e "\e[32m>>>>>>>> configure YUM repos for rabbitmq <<<<<<<<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+echo -e "\e[32m>>>>>>>> Install rabbitmq server <<<<<<<<<<<<<<\e[0m"
 yum install rabbitmq-server -y
-yum install rabbitmq-server -y
+echo -e "\e[32m>>>>>>>> Enable and start rabbitmq <<<<<<<<<<<<<<\e[0m"
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+echo -e "\e[32m>>>>>>>> add application user and pwd <<<<<<<<<<<<<<\e[0m"
 rabbitmqctl add_user roboshop roboshop123
+echo -e "\e[32m>>>>>>>> Set permissions for application user <<<<<<<<<<<<<<\e[0m"
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
