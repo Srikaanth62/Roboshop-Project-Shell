d4fb7d64df7272d4892f3c576be7d82cf874730f
srikaanth <srikaanth62@gmail.com>
2023-04-20 21:00:35 +0530

    cart common

--- cart.sh
+++ cart.sh
@@ -1,9 +1,12 @@
+path=$(realpath "$0")
+script_path=$(dirname "$path")
+source $script_path/common.sh
 echo -e "\e[35m>>>>>>> Setup nodejs repos <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[35m>>>>>> Install nodejs <<<<<<<<<<<\e[0m"
 yum install nodejs -y
 echo -e "\e[35m>>>>>> add application user <<<<<<<<<<<\e[0m"
-useradd roboshop
+useradd ${add_user}
 echo -e "\e[35m>>>>>> create app directory <<<<<<<<<<<\e[0m"
 rm -rf /app
 mkdir /app
@@ -15,7 +18,7 @@ unzip /tmp/cart.zip
 echo -e "\e[35m>>>>>> Install dependencies <<<<<<<<<<<\e[0m"
 npm install
 echo -e "\e[35m>>>>>> copy cart service file <<<<<<<<<<<\e[0m"
-cp /root/Roboshop-Project-Shell/cart.service /etc/systemd/system/cart.service
+cp $script_path/cart.service /etc/systemd/system/cart.service
 echo -e "\e[35m>>>>>> load service <<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 echo -e "\e[35m>>>>>> Enable and start service <<<<<<<<<<<\e[0m"
