af61145608148650585d6935ca8a0d23b6c26fad
srikaanth <srikaanth62@gmail.com>
2023-04-20 14:19:39 +0530

    common

--- user.sh
+++ user.sh
@@ -1,7 +1,5 @@
 script_path=$(pwd)
 source ${script_path}/common.sh
-echo $add_user
-exit
 echo -e "\e[34m>>>>>>>> Setup nodejs repo <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[34m>>>>>>>> Install nodejs <<<<<<<<<<<\e[0m"
