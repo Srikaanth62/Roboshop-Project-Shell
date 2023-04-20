d8550216b161b3e909cf156cca04ae0251b96bdb
srikaanth <srikaanth62@gmail.com>
2023-04-20 18:57:21 +0530

    common

--- user.sh
+++ user.sh
@@ -1,5 +1,7 @@
-script_path=$(dirname $0)
-source ${script_path}/common.sh
+
+source common.sh
+dirname $0
+exit
 echo -e "\e[34m>>>>>>>> Setup nodejs repo <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[34m>>>>>>>> Install nodejs <<<<<<<<<<<\e[0m"
