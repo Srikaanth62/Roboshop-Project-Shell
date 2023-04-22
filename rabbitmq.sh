dd614751c50de3ceb13b6b29c9d347a7f4db9c64
srikaanth <srikaanth62@gmail.com>
2023-04-22 23:11:12 +0530

    cart common

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,21 +6,26 @@ if [ -z "$rabbitmq_user_pwd" ]; then
   echo RabbitMQ Password is missing
   exit
   fi
-print_head() {
-  echo -e "\e[34m>>>>>>>>>>> $1 <<<<<<<<<<<<<<\e[0m"
-}
+
 print_head "Install YUM repo file "
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>>$log_file
+func_exit_code $?
 print_head "Install erlang"
-yum install erlang -y
+yum install erlang -y  &>>$log_file
+func_exit_code $?
 print_head "configure YUM repos for rabbitmq"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>>$log_file
+func_exit_code $?
 print_head "Install rabbitmq server"
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y  &>>$log_file
+func_exit_code $?
 print_head "Enable and start rabbitmq "
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server  &>>$log_file
+systemctl start rabbitmq-server  &>>$log_file
+func_exit_code $?
 print_head "add application user and pwd"
-rabbitmqctl add_user roboshop ${rabbitmq_user_pwd}
+rabbitmqctl add_user roboshop ${rabbitmq_user_pwd}  &>>$log_file
+func_exit_code $?
 print_head "Set permissions for application user "
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$log_file
+func_exit_code $?
