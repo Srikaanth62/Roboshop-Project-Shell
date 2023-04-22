b67980ba7d09dd7a87bee987e95883371dc31cfe
srikaanth <srikaanth62@gmail.com>
2023-04-22 11:49:11 +0530

    cart common

--- common.sh
+++ common.sh
@@ -17,28 +17,28 @@ setup_schema() {
 fi
 }
 func_nodejs() {
-  Print_head "Setup nodejs repos"
+  print_head "Setup nodejs repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-  Print_head "Install nodejs"
+  print_head "Install nodejs"
   yum install nodejs -y
-  Print_head "add application user"
+  print_head "add application user"
   useradd ${add_user}
-  Print_head "create app directory"
+  print_head "create app directory"
   rm -rf /app
   mkdir /app
-  Print_head "Download the application code"
+  print_head "Download the application code"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
-  Print_head "unzip code file "
+  print_head "unzip code file "
   unzip /tmp/${component}.zip
-  Print_head "Install dependencies "
+  print_head "Install dependencies "
   npm install
-  Print_head "copy cart service file"
+  print_head "copy cart service file"
   cp $script_path/${component}.service /etc/systemd/system/${component}.service
-  Print_head "load service "
+  print_head "load service "
   systemctl daemon-reload
-  Print_head "Enable and start service"
+  print_head "Enable and start service"
   systemctl enable ${component}
   systemctl restart ${component}
+  setup_schema
 }
-setup_schema()
