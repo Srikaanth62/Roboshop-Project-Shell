ef7d6f4938d8e6a3b8cbc4e78444731d1611e61a
srikaanth <srikaanth62@gmail.com>
2023-04-22 22:24:59 +0530

    cart common

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,26 @@
 path=$(realpath "$0")
 script_path=$(dirname "$path")
 source ${script_path}/common.sh
-print_head() {
-  echo -e "\e[33m>>>>>>>>> $1 <<<<<<<<<<\e[0m"
-}
-print_head "Install Nginx "
-yum install nginx -y
-print_head "Download frontend source code "
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-print_head "Remove html folder"
-rm -rf /usr/share/nginx/html/*
-print_head "change path to html "
-cd /usr/share/nginx/html
-print_head "unzip frontend folder "
-unzip /tmp/frontend.zip
-print_head "copy roboshop conf file"
-cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf
-print_head "enable and start nginx "
-systemctl enable nginx
-systemctl restart nginx
 
+  print_head "Install Nginx "
+  yum install nginx -y  &>>$log_file
+  func_exit_code
+  print_head "Download frontend source code "
+  curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>$log_file
+  func_exit_code
+  print_head "Remove html folder"
+  rm -rf /usr/share/nginx/html/*  &>>$log_file
+  func_exit_code
+  print_head "change path to html "
+  cd /usr/share/nginx/html  &>>$log_file
+  func_exit_code
+  print_head "unzip frontend folder "
+  unzip /tmp/frontend.zip  &>>$log_file
+  func_exit_code
+  print_head "copy roboshop conf file"
+  cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf  &>>$log_file
+  func_exit_code
+  print_head "enable and start nginx "
+  systemctl enable nginx  &>>$log_file
+  systemctl restart nginx  &>>$log_file
+  func_exit_code
