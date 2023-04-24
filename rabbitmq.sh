9e4811f1f5ddcd41c0e71491bf7feb4ff768845a
srikaanth <srikaanth62@gmail.com>
2023-04-24 20:49:48 +0530

    cart common

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,12 @@
 path=$(realpath "$0")
 script_path=$(dirname "$path")
 source ${script_path}/common.sh
-id ${app_user} &>>$log_file
+
+# Check if user "roboshop" exists
+if id "roboshop" >/dev/null 2>&1; then
+    echo "User 'roboshop' already exists."
+fi
+
 rabbitmq_user_pwd=$1
 if [ -z "$rabbitmq_user_pwd" ]; then
   echo RabbitMQ Password is missing
