89fc597e0e8ccb3d2e681b279750d8cdeb954e7d
srikaanth <srikaanth62@gmail.com>
2023-04-24 16:45:20 +0530

    cart common

--- payment.sh
+++ payment.sh
@@ -1,9 +1,9 @@
 path=$(realpath "$0")
 script_path=$(dirname "$path")
 source ${script_path}/common.sh
+component=payment
 rabbitmq_user_pwd=$1
 if [ -z "$rabbitmq_user_pwd" ]; then
   echo RabbitMQ password is missing
   exit
   fi
-component=payment
