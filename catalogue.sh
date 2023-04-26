ae319d2f6eed94f350be253ec2c3056c4e55ff6a
srikaanth <srikaanth62@gmail.com>
2023-04-26 11:20:45 +0530

    cart common

--- catalogue.sh
+++ catalogue.sh
@@ -14,7 +14,7 @@ systemctl enable catalogue
 systemctl start catalogue
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
-mongo --host mongodb.srikaanth62.online </app/schema/catalogue.js
+mongo --host mongodb-dev.srikaanth62.online </app/schema/catalogue.js
 systemctl restart catalogue
 
 
