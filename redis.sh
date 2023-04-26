a711140f97bfbaaa21b511163800a1c05fb37220
srikaanth <srikaanth62@gmail.com>
2023-04-26 10:28:57 +0530

    cart common

--- redis.sh
+++ redis.sh
@@ -17,6 +17,7 @@ func_exit_code $?
 ## Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
 print_head "Enable and restart the redis service"
 systemctl enable redis  &>>$log_file
+systemctl start redis  &>>$log_file
 systemctl restart redis  &>>$log_file
 func_exit_code $?
 
