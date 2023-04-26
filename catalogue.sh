0d0e69be8bb06482dae13277e755bbd0d2e0f682
srikaanth <srikaanth62@gmail.com>
2023-04-26 11:14:42 +0530

    cart common

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,22 @@
-path=$(realpath "$0")
-script_path=$(dirname "$path")
-source ${script_path}/common.sh
-component=catalogue
-setup_schema=mongo
-func_nodejs
+#!/bin/bash
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cp catalogue.service /etc/systemd/system/catalogue.service
+cd /app
+unzip /tmp/catalogue.zip
+npm install
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb.srikaanth62.online </app/schema/catalogue.js
+systemctl restart catalogue
+
 
 
 
