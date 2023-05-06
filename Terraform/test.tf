3f1c31fe1a8fb3c542a01c0c0b82be4a413ef06d
srikaanth <srikaanth62@gmail.com>
2023-05-06 07:50:03 +0530

    cart common

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -1,11 +1,17 @@
+data "aws_ami" "centos" {
+  most_recent      = true
+  name_regex       = "Devops-Practice"
+  owners           = ["973714476881"]
+}
+/*
 resource "aws_instance" "Test" {
   ami = "ami-0bb6af715826253bf"
   instance_type = "t3.micro"
   tags = {
     Name = "Terraform"
   }
-}
+} */
 
-output "Public" {
-  value = aws_instance.Test.public_ip
+output "ami" {
+  value = data.aws_ami.centos.image_id
 }
