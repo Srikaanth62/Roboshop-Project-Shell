ba3f085a556df0c3ee7e737ebf02ad13e8bd9bf5
srikaanth <srikaanth62@gmail.com>
2023-05-06 07:55:33 +0530

    terraform example

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -1,17 +1,18 @@
 data "aws_ami" "centos" {
   owners           = ["973714476881"]
   most_recent      = true
-  name_regex       = "Centos-7-DevOps-Practice"
+  name_regex       = "Centos-8-DevOps-Practice"
 }
-/*
+
 resource "aws_instance" "Test" {
-  ami = "ami-0bb6af715826253bf"
+  ami = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   tags = {
     Name = "Terraform"
   }
-} */
+}
 
+/*
 output "ami" {
   value = data.aws_ami.centos.image_id
-}
+} */
