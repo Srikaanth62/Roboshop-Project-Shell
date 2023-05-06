177ae8d5ab7fb2027c56092acf44fbe250c160db
srikaanth <srikaanth62@gmail.com>
2023-05-06 07:52:16 +0530

    cart common

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "centos" {
-  most_recent      = true
-  name_regex       = "Devops-Practice"
   owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-7-DevOps-Practice"
 }
 /*
 resource "aws_instance" "Test" {
