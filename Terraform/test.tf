c10f5fecc7d0601505d725b907ff548676128830
srikaanth <srikaanth62@gmail.com>
2023-05-06 20:32:50 +0530

    terraform example

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -85,6 +85,10 @@ resource "null_resource" "fruits" {
   count = length(var.fruits)
 }
 
+provisioner "local-exec" {
+  command = "echo fruits name ${var.fruits[count.index]}"
+}
+
 
 variable "fruits" {
   default = [ "apple", "banana", "orange" ]
