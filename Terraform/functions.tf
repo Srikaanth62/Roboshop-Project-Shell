5270db765cd2eac40bd438541cb16a166271219b
srikaanth <srikaanth62@gmail.com>
2023-05-07 19:32:36 +0530

    terraform example

--- Terraform/functions.tf
+++ Terraform/functions.tf
@@ -5,3 +5,11 @@ variable "sample" {
 output "name" {
   value = upper(var.sample)
 }
+
+variable "sample1" {
+  default = [ "sri", "kanth" ]
+}
+
+output "sample1" {
+  value = element (var.sample1,0)
+}
