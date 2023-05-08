cd5dd3bcd9238a6e3ca6605b93a1246c1c6091ad
srikaanth <srikaanth62@gmail.com>
2023-05-08 17:43:01 +0530

    terraform example

--- module/example/sample.tf
+++ module/example/sample.tf
@@ -3,3 +3,19 @@ resource "null_resource" "null" {
     command = "echo name is srikaanth"
   }
 }
+
+resource "null_resource" "null1" {
+  provisioner "local-exec" {
+    command = "echo name is srikaanth"
+  }
+}
+resource "null_resource" "null2" {
+  provisioner "local-exec" {
+    command = "echo name is srikaanth"
+  }
+}
+resource "null_resource" "null3" {
+  provisioner "local-exec" {
+    command = "echo name is srikaanth"
+  }
+}
